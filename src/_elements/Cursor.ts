bd1ec7fb9360cc9d1b05619626fc3b638431f899
Georgiy Malinovskiy <post@gmalinovskiy.su>
2021-10-25 16:35:24 +0300

    Cursor pointer

--- src/_elements/Cursor.ts
+++ src/_elements/Cursor.ts
@@ -1,24 +1,33 @@
-import { implementsElement } from './_utils';
+import { implementsElement, MIN_INTERACTIVE_SIZE } from './_utils';
 import ElementComponent from './_utils/ElementComponent';
 
 @implementsElement()
 class Cursor extends ElementComponent {
-  size: number;
+  shape: [number, number][];
 
   constructor(...args: ConstructorParameters<typeof ElementComponent>) {
     super(...args);
-    this.size = 10;
+    const [p] = args;
+
+    this.shape = [
+      [MIN_INTERACTIVE_SIZE / 2, 0],
+      [MIN_INTERACTIVE_SIZE, MIN_INTERACTIVE_SIZE],
+      [0, MIN_INTERACTIVE_SIZE],
+    ];
   }
 
   draw() {
-    const { p, gpContext: { gp2d }, size } = this;
+    const { p, gpContext: { gp2d } } = this;
 
     gp2d.fill(255);
+    gp2d.push();
+    gp2d.translate(p.mouseX, p.mouseY);
     gp2d.beginShape();
-    gp2d.vertex(p.mouseX, p.mouseY - size / 2);
-    gp2d.vertex(p.mouseX + size, p.mouseY + size);
-    gp2d.vertex(p.mouseX - size, p.mouseY + size);
+    this.shape.forEach((coords) => {
+      gp2d.vertex(...coords);
+    });
     gp2d.endShape(gp2d.CLOSE);
+    gp2d.pop();
   }
 }
 
